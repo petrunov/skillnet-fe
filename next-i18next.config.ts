e84ade714557eb98186746bf205aa65a590a9ff6
Vesselin Petrunov <petrunov@protonmail.com>
2025-04-23 22:06:31 +0300

    Add multilanguage capabilities

---
+++ next-i18next.config.ts
@@ -0,0 +1,10 @@
+import type { UserConfig } from 'next-i18next';
+
+const config: UserConfig = {
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'bg'],
+  },
+};
+
+export default config;
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+import type { I18NConfig } from 'next/dist/server/config-shared';
+import i18nConfig from './next-i18next.config';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  i18n: i18nConfig.i18n as I18NConfig,
+  // other config options here
 };
 
 export default nextConfig;
